4daa6eb539e9260ef67ba11000978378b51206f0
Moe <github@m03.ca>
2016-12-03 16:23:36 -0800

    readme

--- camera.js
+++ camera.js
@@ -1,3 +1,22 @@
+//
+// Shinobi
+// Copyright (C) 2016-2025 Moe Alam
+// Please Donate if you consider this platform for commercial purposes :)
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
 var fs = require('fs');
 var mysql = require('mysql');
 var moment = require('moment');
