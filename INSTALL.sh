e932cfe087bd3d3a56b403a814fe828cf04ed633
Moe <github@m03.ca>
2016-12-22 22:51:24 -0800

    simplify installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,14 +9,13 @@ ln -s /usr/bin/nodejs /usr/bin/node
 chmod -R 755 .
 
 echo "Shinobi - Database Installation"
-echo "What is your SQL Username"
-read sqluser
-echo "What is your SQL Password"
+echo "What is your SQL Password?"
+echo "**You set this just a few moments ago if MySQL was installed during this installer."
 read sqlpass
 
-mysql -u $sqluser -p$sqlpass -e "source sql/user.sql" || true
-mysql -u $sqluser -p$sqlpass -e "source sql/framework.sql" || true
-mysql -u $sqluser -p$sqlpass --database ccio -e "source sql/default_data.sql" || true
+mysql -u root -p$sqlpass -e "source sql/user.sql" || true
+mysql -u root -p$sqlpass -e "source sql/framework.sql" || true
+mysql -u root -p$sqlpass --database ccio -e "source sql/default_data.sql" || true
 echo "Shinobi - Install NPM Libraries"
 npm install
 echo "Shinobi - Install PM2"
