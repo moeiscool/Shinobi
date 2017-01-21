18065849e114ca1469bfcc7647ae6dd66eb1e9f4
Christos <christos342@gmail.com>
2017-01-21 23:03:48 +0200

    Make a proper updater

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-rm -rf master master_temp;
-wget https://github.com/moeiscool/Shinobi/tarball/master;
-mkdir master_temp;
-tar -xzf master -C master_temp --strip-components=1;
-rm -rf camera.js web UPDATE.sh package.json;
-mv master_temp/UPDATE.sh UPDATE.sh;
-mv master_temp/web web;
-mv master_temp/package.json package.json;
-mv master_temp/camera.js camera.js;
+rm -rf master master_temp
+wget https://github.com/moeiscool/Shinobi/tarball/master
+mkdir master_temp
+tar -xzf master -C master_temp --strip-components=1
+rm -rf camera.js web UPDATE.sh package.json
+pm2 stop camera.js
+pm2 kill
+mv master_temp/UPDATE.sh UPDATE.sh
+mv master_temp/web web
+mv master_temp/package.json package.json
+mv master_temp/camera.js camera.js
 npm install
-pm2 restart camera.js;
-rm -rf master master_temp;
+rm -rf master master_temp
+pm2 start camera.js
