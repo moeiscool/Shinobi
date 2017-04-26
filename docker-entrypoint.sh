15d3b9ff258e2bcd70d86c9d247b003ce7484eea
Moe <github@m03.ca>
2017-04-26 16:19:18 -0700

    docker trying

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,5 @@ source $SHIN_BIN_DIR/docker.env
 sed -i 's/"host": "127.0.0.1"/"host": "shinobi-db"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"user": "majesticflame"/"user": "'${MYSQL_USER}'"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"password": ""/"password": "'${MYSQL_PASSWORD}'"/g' $SHIN_BIN_DIR/conf.json
-pm2 start /opt/shinobi/camera.js
 pm2 start /opt/shinobi/cron.js
+/usr/bin/nodejs camera.js
