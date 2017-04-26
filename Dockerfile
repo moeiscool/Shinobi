9f4c81bcf19179114183afa4f9813c55972c7d6d
Moe <github@m03.ca>
2017-04-26 15:37:01 -0700

    docker needs to be in main dir, test

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apt-get update \
     && apt-get upgrade -y \
     && apt-get install -y ffmpeg nodejs npm libav-tools
 RUN ln -s /usr/bin/nodejs /usr/bin/node && mkdir /opt/shinobi
-ADD ./../../ /opt/shinobi
+ADD . /opt/shinobi
 WORKDIR /opt/shinobi
 RUN cp /opt/shinobi/conf.sample.json /opt/shinobi/conf.json
 RUN npm install pm2 -g
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SHIN_BIN_DIR=/opt/shinobi
-source $SHIN_BIN_DIR/INSTALL/docker/docker.env
+source $SHIN_BIN_DIR/docker.env
 sed -i 's/"host": "127.0.0.1"/"host": "shinobi-db"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"user": "majesticflame"/"user": "'${MYSQL_USER}'"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"password": ""/"password": "'${MYSQL_PASSWORD}'"/g' $SHIN_BIN_DIR/conf.json
