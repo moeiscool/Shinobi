654df455d019493935e5dc25188fa5d122426a70
Moe <github@m03.ca>
2017-04-27 12:46:44 -0700

    moved commands to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,13 @@ RUN echo 'mysql-server mysql-server/root_password password night' | debconf-set-
 RUN echo 'mysql-server mysql-server/root_password_again password night' | debconf-set-selections
 RUN apt -y install mysql-server --no-install-recommends
 RUN sed -ie "s/^bind-address\s*=\s*127\.0\.0\.1$/#bind-address = 127.0.0.1/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
 RUN ln -s /usr/bin/nodejs /usr/bin/node
 RUN mkdir /opt/shinobi
 COPY . /opt/shinobi
 RUN cp /opt/shinobi/conf.sample.json /opt/shinobi/conf.json
 RUN chmod -R 755 /opt/shinobi
 WORKDIR /opt/shinobi
-RUN cp /opt/shinobi/INSTALL/docker-install.sh /tmp/db.sh
-RUN chmod +x /tmp/db.sh
-RUN /tmp/db.sh
 RUN npm install
 RUN npm install pm2 -g
 RUN chmod +x ./docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 SHIN_BIN_DIR=/opt/shinobi
 source $SHIN_BIN_DIR/docker.env
+
+
+if [ ! -e "/opt/shinobi/installed.txt" ]; then
+    touch /opt/shinobi/installed.txt
+    /etc/init.d/mysql start
+    /usr/bin/mysqld_safe &
+    sleep 5
+    mysql -u root -pnight -e "CREATE USER 'root'@'192.168.99.1' IDENTIFIED BY 'night';" || true
+    mysql -u root -pnight -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'192.168.99.1';" || true
+    mysql -u root -pnight -e "FLUSH PRIVILEGES;" || true
+    #mysql -u root -pnight < /opt/shinobi/sql/user.sql || true
+    mysql -u root -pnight < /opt/shinobi/sql/framework.sql || true
+    mysql -u root -pnight --database ccio < /opt/shinobi/sql/default_data.sql || true
+fi
+
+
 sed -i 's/"user": "majesticflame"/"user": "'${MYSQL_USER}'"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"password": ""/"password": "'${MYSQL_PASSWORD}'"/g' $SHIN_BIN_DIR/conf.json
 npm cache clean -f && npm install -g n && n stable
