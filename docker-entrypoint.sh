739e841195214ab65f2a5d0189c12ca6b8746b6a
Moe <github@m03.ca>
2017-04-27 13:15:12 -0700

    Docker installs and runs, added motion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,20 +2,19 @@
 SHIN_BIN_DIR=/opt/shinobi
 source $SHIN_BIN_DIR/docker.env
 
+/etc/init.d/mysql start
+/usr/bin/mysqld_safe &
+sleep 5
 
 if [ ! -e "/opt/shinobi/installed.txt" ]; then
     touch /opt/shinobi/installed.txt
-    /etc/init.d/mysql start
-    /usr/bin/mysqld_safe &
-    sleep 5
-    mysql -u root -pnight -e "CREATE USER 'root'@'192.168.99.1' IDENTIFIED BY 'night';" || true
-    mysql -u root -pnight -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'192.168.99.1';" || true
-    mysql -u root -pnight -e "FLUSH PRIVILEGES;" || true
-    #mysql -u root -pnight < /opt/shinobi/sql/user.sql || true
-    mysql -u root -pnight < /opt/shinobi/sql/framework.sql || true
-    mysql -u root -pnight --database ccio < /opt/shinobi/sql/default_data.sql || true
+    mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE USER '${MYSQL_USER}'@'192.168.99.1' IDENTIFIED BY '${MYSQL_PASSWORD}';" || true
+    mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_USER}'@'192.168.99.1';" || true
+    mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "FLUSH PRIVILEGES;" || true
+    #mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} < /opt/shinobi/sql/user.sql || true
+    mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} < /opt/shinobi/sql/framework.sql || true
+    mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --database ccio < /opt/shinobi/sql/default_data.sql || true
 fi
-
 
 sed -i 's/"user": "majesticflame"/"user": "'${MYSQL_USER}'"/g' $SHIN_BIN_DIR/conf.json
 sed -i 's/"password": ""/"password": "'${MYSQL_PASSWORD}'"/g' $SHIN_BIN_DIR/conf.json
@@ -24,5 +23,5 @@ cd /opt/shinobi
 npm install
 pm2 start /opt/shinobi/cron.js
 pm2 start /opt/shinobi/camera.js
-#pm2 start /opt/shinobi/plugins/motion/shinobi-motion.js
+pm2 start /opt/shinobi/plugins/motion/shinobi-motion.js
 pm2 logs
