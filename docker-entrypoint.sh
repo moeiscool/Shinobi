7f774e0d8d21828cac911b19af47cc3737dcdd01
Alexander Simonov <alex@simonov.me>
2017-05-12 14:57:29 +0300

    Add new variables for custom links of mysql

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 SHIN_BIN_DIR=/opt/shinobi
+MYSQL_ROOT_USER="${MYSQL_ROOT_USER:-root}"
+MYSQL_HOST="${MYSQL_HOST:-mysql}"
 cd /opt/shinobi
 
 if [ ! -e "/opt/shinobi/installed.txt" ]; then
 #install stuff if not installed
-    my_mysql="/usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql"
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql -e 'FLUSH PRIVILEGES ;'
-    #mysql -u root -pnight < /opt/shinobi/sql/user.sql || true
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql --database $MYSQL_DATABASE < /opt/shinobi/sql/framework.sql
-    /usr/bin/mysql -u root -p${MYSQL_ROOT_PASSWORD} -h mysql --database $MYSQL_DATABASE < /opt/shinobi/sql/default_data.sql
+    my_mysql="/usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST}"
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e 'FLUSH PRIVILEGES ;'
+    #mysql -u ${MYSQL_ROOT_USER} -pnight < /opt/shinobi/sql/user.sql || true
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} --database $MYSQL_DATABASE < /opt/shinobi/sql/framework.sql
+    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} --database $MYSQL_DATABASE < /opt/shinobi/sql/default_data.sql
     sed -i 's/"user": "majesticflame"/"user": "'"${MYSQL_USER}"'"/g' $SHIN_BIN_DIR/conf.json
     sed -i 's/"password": ""/"password": "'"${MYSQL_PASSWORD}"'"/g' $SHIN_BIN_DIR/conf.json
     sed -i 's/"host": "127.0.0.1"/"host": "mysql"/g' $SHIN_BIN_DIR/conf.json
