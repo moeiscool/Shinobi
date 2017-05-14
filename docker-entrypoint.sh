696c2f5020c2600a5991e2749fd9ae837e0caa5d
Moe <github@m03.ca>
2017-05-13 18:30:56 -0700

    revert docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 SHIN_BIN_DIR=/opt/shinobi
-MYSQL_USER="${MYSQL_USER:-ccio}"
-MYSQL_ROOT_USER="${MYSQL_ROOT_USER:-root}"
-MYSQL_HOST="${MYSQL_HOST:-mysql}"
-MYSQL_PASSWORD="${MYSQL_PASSWORD:-testpass}"
-cd /opt/shinobi
-tables_check="select count(*) from information_schema.tables where table_schema='API' and table_name='${MYSQL_DATABASE}';"
-tables_num=$(/usr/bin/mysql -N -s -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "${tables_check}")
-echo $tables_num
+source $SHIN_BIN_DIR/docker.env
 
-if [ "${tables_num}" -eq "0" ]; then
-     #install stuff if not installed
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e 'FLUSH PRIVILEGES;'
-    #mysql -u ${MYSQL_ROOT_USER} -pnight < /opt/shinobi/sql/user.sql || true
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} --database $MYSQL_DATABASE < /opt/shinobi/sql/framework.sql
-    /usr/bin/mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} --database $MYSQL_DATABASE < /opt/shinobi/sql/default_data.sql
-    sed -i 's/"user": "majesticflame"/"user": "'"${MYSQL_USER}"'"/g' $SHIN_BIN_DIR/conf.json
-    sed -i 's/"password": ""/"password": "'"${MYSQL_PASSWORD}"'"/g' $SHIN_BIN_DIR/conf.json
-    sed -i 's/"host": "127.0.0.1"/"host": "'"${MYSQL_HOST}"'"/g' $SHIN_BIN_DIR/conf.json
-    sed -i 's/"database": "ccio"/"database": "'"${MYSQL_DATABASE}"'"/g' $SHIN_BIN_DIR/conf.json
+/etc/init.d/mysql start
+sleep 2
+cd /opt/shinobi
+
+if [ ! -e "/opt/shinobi/installed.txt" ]; then
+#install stuff if not installed
+    touch /opt/shinobi/installed.txt
+    mysql -u root -pnight -e "CREATE USER 'root'@'192.168.99.1' IDENTIFIED BY 'night';" || true
+    mysql -u root -pnight -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'192.168.99.1';" || true
+    mysql -u root -pnight -e "FLUSH PRIVILEGES;" || true
+    #mysql -u root -pnight < /opt/shinobi/sql/user.sql || true
+    mysql -u root -pnight < /opt/shinobi/sql/framework.sql || true
+    mysql -u root -pnight --database ccio < /opt/shinobi/sql/default_data.sql || true
+    sed -i 's/"user": "majesticflame"/"user": "root"/g' $SHIN_BIN_DIR/conf.json
+    sed -i 's/"password": ""/"password": "night"/g' $SHIN_BIN_DIR/conf.json
+    sed -i 's/"port":3306/"port":3314/g' $SHIN_BIN_DIR/conf.json
+    sed -i 's/"port": 8080/"port": 8083/g' $SHIN_BIN_DIR/conf.json
+    sed -i 's/"port":8080/"port":8083/g' $SHIN_BIN_DIR/plugins/motion/conf.json
     npm cache clean -f && npm install -g n && n stable
+else
+#update if already installed
+    wget https://github.com/moeiscool/Shinobi/tarball/master
+    mkdir master_temp
+    tar -xzf master -C master_temp --strip-components=1
+    rm -rf camera.js web UPDATE.sh package.json cron.js
+    mv master_temp/UPDATE.sh UPDATE.sh
+    chmod +x UPDATE.sh
+    mv master_temp/web web
+    mv master_temp/package.json package.json
+    mv master_temp/camera.js camera.js
+    mv master_temp/cron.js cron.js
+    mv master_temp/plugins/motion/shinobi-motion.js plugins/motion/shinobi-motion.js
+    npm install
+    rm -rf master master_temp
 fi
 
 pm2 start /opt/shinobi/cron.js
