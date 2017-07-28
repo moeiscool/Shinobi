33816f9cbc052fcfbbc8f0ac3baf7e22e908b8ae
Moe <github@m03.ca>
2017-07-27 23:28:29 -0700

    update UPDATE.sh

--- UPDATE.sh
+++ UPDATE.sh
@@ -3,7 +3,7 @@ rm -rf master master_temp
 wget https://github.com/moeiscool/Shinobi/tarball/master
 mkdir master_temp
 tar -xzf master -C master_temp --strip-components=1
-rm -rf camera.js web UPDATE.sh package.json cron.js languages
+rm -rf camera.js web UPDATE.sh package.json cron.js languages LICENSE COPYING
 pm2 stop camera.js
 pm2 stop cron.js
 pm2 kill
@@ -11,6 +11,8 @@ mv master_temp/UPDATE.sh UPDATE.sh
 chmod +x UPDATE.sh
 mv master_temp/languages languages
 mv master_temp/web web
+mv master_temp/LICENSE LICENSE
+mv master_temp/COPYING COPYING
 mv master_temp/package.json package.json
 mv master_temp/camera.js camera.js
 mv master_temp/cron.js cron.js
